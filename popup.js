742971700155171ecbbfba8143f9827dc7f411b9
Grace Newman <grace.newman@digital.hmrc.gov.uk>
2018-03-05 09:47:36 +0000

    Added scraper and bundled

--- popup.js
+++ popup.js
@@ -1,9 +1,33 @@
+var request = require('request');
+var cheerio = require('cheerio');
+const accountSid = 'AC8081e11c434cf78d7337089f667b86d8';
+const authToken = '526fe5700e5f123196b939cbbfe04332';
+const client = require('twilio')(accountSid, authToken);
+
 function click(e) {
  chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
    function(tabs){
      page = tabs[0].url;
-   }
-);
+     phone = document.getElementById('number').value;
+     alert(phone);
+    // request(page, function (error, response, html) {
+    //   if (!error && response.statusCode == 200) {
+    //     var $ = cheerio.load(html);
+    //     $('.story-body__inner p').each(function(i, element){
+    //       var a = $(this).prev();
+    //       var data = a.text();
+    //       // console.log(data);
+    //       client.messages.create(
+    //         {
+    //           body: data ,
+    //           to: phone,
+    //           from: '441692252053',
+    //         })
+    //         .then((message) => alert(message.sid));
+    //     });
+    //   }
+    // });
+   });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
