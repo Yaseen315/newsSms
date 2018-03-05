f2f89b7963fc8e1c18b401356bc4940d00a9ef34
Grace Newman <grace.newman@digital.hmrc.gov.uk>
2018-03-05 09:49:41 +0000

    Updated popup.js

--- popup.js
+++ popup.js
@@ -9,24 +9,23 @@ function click(e) {
    function(tabs){
      page = tabs[0].url;
      phone = document.getElementById('number').value;
-     alert(phone);
-    // request(page, function (error, response, html) {
-    //   if (!error && response.statusCode == 200) {
-    //     var $ = cheerio.load(html);
-    //     $('.story-body__inner p').each(function(i, element){
-    //       var a = $(this).prev();
-    //       var data = a.text();
-    //       // console.log(data);
-    //       client.messages.create(
-    //         {
-    //           body: data ,
-    //           to: phone,
-    //           from: '441692252053',
-    //         })
-    //         .then((message) => alert(message.sid));
-    //     });
-    //   }
-    // });
+     request(page, function (error, response, html) {
+       if (!error && response.statusCode == 200) {
+         var $ = cheerio.load(html);
+         $('.story-body__inner p').each(function(i, element){
+           var a = $(this).prev();
+           var data = a.text();
+           // console.log(data);
+           client.messages.create(
+             {
+               body: data ,
+               to: phone,
+               from: '441692252053',
+             })
+             .then((message) => alert(message.sid));
+          });
+        }
+      });
    });
 }
 
